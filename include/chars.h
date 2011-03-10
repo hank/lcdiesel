88aa3fe6bc8cbfef2a3909c39cea78636634b92a
Erik Gregg <ralree@gmail.com>
2011-03-10 11:57:35 -0500

    Licensing on up!

--- include/chars.h
+++ include/chars.h
@@ -1,9 +1,38 @@
-/* Defines custom characters.  To use, just #define CHAR_USE_x where x is the
- * name of the desired character in all caps.  Then access it in your program
- * with _char_x where x is the lowercase name.
- * 
- * The reason these are not enabled by default is to keep program size down.
- */
+/*  Defines custom characters.  
+    Copyright 2010 Erik Gregg. All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY ERIK GREGG ``AS IS'' AND ANY EXPRESS
+OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL ERIK GREGG OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+    The views and conclusions contained in the software and documentation are
+those of the authors and should not be interpreted as representing official
+policies, either expressed or implied, of Erik Gregg.
+*/
+/* To use, just #define CHAR_USE_x where x is the
+   name of the desired character in all caps.  Then access it in your program
+   with _char_x where x is the lowercase name.
+   
+   The reason these are not enabled by default is to keep program size down.
+   For instance, the ATtiny2313 only has 2K of flash :)
+*/
 #ifndef CHARS_H
 #define CHARS_H
 
--- include/lcd.h
+++ include/lcd.h
@@ -1,24 +1,43 @@
 #ifndef LCD_H
 #define LCD_H
-/**
-Basic routines for interfacing a HD44780U-based text LCD display
+/*  Basic routines for interfacing a HD44780U-based text LCD display
+    Copyright (C) 2006 Peter Fleury
 
-Originally based on Volker Oth's LCD library,
-changed lcd_init(), added additional constants for lcd_command(), 
-added 4-bit I/O mode, improved and optimized code.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+/**
+Peter Fleury pfleury@gmx.ch http://jump.to/fleury
+Modified by Erik Gregg ralree@gmail.com http://www.ralree.com
+ 
+History
+    Gregg:
+      Based on Peter Fleury's library, retaining GPL license.
+      Added custom character commands, cleaned a few things up.
+
+    Fleury:
+      Originally based on Volker Oth's LCD library,
+      changed lcd_init(), added additional constants for lcd_command(), 
+      added 4-bit I/O mode, improved and optimized code.
       
+Description
+
 Library can be operated in memory mapped mode (LCD_IO_MODE=0) or in 
 4-bit IO port mode (LCD_IO_MODE=1). 8-bit IO port mode not supported.
 
 Memory mapped mode compatible with Kanda STK200, but supports also 
 generation of R/W signal through A8 address line.
-      
-Peter Fleury pfleury@gmx.ch http://jump.to/fleury
-Modified by Erik Gregg ralree@gmail.com http://www.ralree.com
- 
-See:
-The chapter http://homepage.sunrise.ch/mysunrise/peterfleury/avr-lcd44780.html
-Interfacing a HD44780 Based LCD to an AVR.
 */
 
 #if (__GNUC__ * 100 + __GNUC_MINOR__) < 303
